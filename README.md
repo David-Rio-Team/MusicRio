a1134a781b6ce06efa13d190f52153e8dd013de9
David-Rio-Team <87323990+David-Rio-Team@users.noreply.github.com>
2021-07-31 05:40:14 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,29 @@
+<h1 align="centre">🎶 Music Bot v-2</h1>
+
+## Requirements 📝
+
+
+
+<h2 align="centre">⚙️ Set Up ⚙️</h3>
+
+<h3 align="centre"> Heroku </h4>
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/David-Rio-Team/MusicRio)
+
+Get STRING_NAME from here:
+
+[![GenerateString](https://img.shields.io/badge/repl.it-generateString-yellowgreen)](https://replit.com/@ZauteKm/generate-pyrogram-session-string#main.py)
+
+### Mandatory Vars.
+
+- Some Of The Mandatory Vars Are :-
+   - `API_ID` :  Give API_ID of your Alternate Telegram Account.
+   - `API_HASH` :  Give API_HASH of your Alternate Telegram Account.
+   - `STRING_NAME` :  Make a string session from [here](https://replit.com/@ZauteKm/generate-pyrogram-session-string#main.py)
+   - `BOT_TOKEN` :  Make a Bot from [@Botfather](https://t.me/botfather) and fill it's bot token.
+   - `SUDO_USERS` :  Fill Userid of yhe users whom you want to be able to control the bot. You can add multiple id by giving a space in b/w each id. Get from [@DingdiBot](http://t.me/DingdiBot)
+
+## Credits
+
+- [Devloper](https://t.me/tsttt): Devloper Rio
+- [Ch Source](https://t.me/L9L9L): Ch Source
---
+++ handlers/private.py
@@ -0,0 +1,83 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+from config import BOT_NAME as bn
+from helpers.filters import other_filters2
+
+
+@Client.on_message(other_filters2)
+async def start(_, message: Message):
+      await message.reply_text(
+        f"""**مرحبا انا **{bn}** 🎵
+
+بامكاني تشغيل الاغاني في المكالمات الجماعيه 
+قم برفعي  مشرف في قناتك مع البوت المساعد [David Music](https://t.me/xRioMusic).
+
+قم باضافتي الى مجموعتك لتبدأ الحفله 🎶**
+        """,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "لطلب المساعده", url="https://t.me/tsttt")
+                  ],[
+                    InlineKeyboardButton(
+                        "قناة للشروحات", url="https://t.me/X6UX6"
+                    ),
+                    InlineKeyboardButton(
+                        "قناة البوت", url="https://t.me/CQCQQ"
+                    )
+                ],[ 
+                    InlineKeyboardButton(
+                        "اضفني الى مجموعتك", url="https://t.me/MusicRioBOT?startgroup=true"
+                    )]
+            ]
+        ),
+     disable_web_page_preview=True
+    )
+
+@Client.on_message(filters.command("start") & ~filters.private & ~filters.channel)
+async def gstart(_, message: Message):
+      await message.reply_text("""** تم تفعيل البوت بنجاح ✅**""",
+      reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "قناتي الخاصه", url="https://t.me/W55555")
+                ]
+            ]
+        )
+   )
+
+
+@Client.on_message(filters.command("help") & ~filters.private & ~filters.channel)
+async def gstart(_, message: Message):
+      await message.reply_text("""** يمتاز هذا البوت بالبحث والتحميل ✨
+اكتب معرف البوت مع اسم الاغنيه للبحث 🔊
+مثال : 
+@MusicRioBOT كاظم الساهر
+تستطيع تحميل اي اغنيه ايضا 
+بالاوامر التاليه :
+- /ytp رابط الاغنيه من اليوتيوب
+- /song رابط الاغنيه من اليوتيوب
+
+للتحكم بالاغنية داخل المكالمه الجماعيه 🔊
+- /play بالرد على الاغنيه او الرابط للتشغيل
+- /pause لايقاف الاغنيه موقتا داخل المكالمه
+- /resume لتكمله الاغنيه المتوقفه
+- /stop لايقاف البوت عن تشغيل الاغنيه
+- /skip لتخطي الاغنيه الحاليه والانتقال الى الاغنيه التاليه
+#ملاحظه : تستطيع ان تقوم بتشغيل اغنيه اخرى فتضاف الى الدور بعد الاغنيه الحاليه فتتنقل بينها وبين الاغاني الباقيه باستخدام امر /skip 🔖 **""",
+      reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "قناتي الخاصه", url="https://t.me/W55555")
+                ],[
+                    InlineKeyboardButton(
+                        "الحساب المساعد", url="https://t.me/xRioMusic"
+                    )
+                ]
+            ]
+        )
+   )
